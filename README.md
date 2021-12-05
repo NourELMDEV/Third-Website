d4398a7ebdad8d80b1a3e948243786cd48e786e4
Nour/Elm <60050784+NourELMDEV@users.noreply.github.com>
2021-12-04 21:54:59 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Third-Website
-My third project which is a pretty nice website based on an "about me"
+# mahbroject
---
+++ index.php
@@ -0,0 +1,72 @@
+<?php
+<head>
+    <meta charset="utf-8">
+    <link rel="icon" href="favicon (2).ico">
+    <link rel="stylesheet" href="css/styles.css">
+       <title>About Me</title>
+       <!--Google Fonts Selected.-->
+       <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Merriweather&family=Montserrat:wght@600&family=Sacramento&display=swap" rel="stylesheet">
+</head>
+<body>
+<div class="top-container">
+    <img class="top-cloud" src="images/cloud.png" alt="cloud-image">
+    <h1>I'm Nour.</h1>
+<h2><span style="text-decoration:underline;">A Beginner</span> Web Dev.</h2>
+<img class="buttom-cloud" src="images/cloud.png" alt="cloud-image">
+<div class="divi">
+<img class="coolcat" src="images/gifcat.gif" alt="">
+<img class="drumcat" src="images/giphy.gif" alt="">
+<img class="coder" src="images/coding.gif" alt="">
+</div>
+
+<hr>
+
+</div>
+
+<div class="middle-container">
+    <div class="profile">
+        <img class="av" src="images/avatar.png" alt="">
+      <h2>Hello.</h2>
+      <p id="p1">I am Nour. I ❤️ Web Dev. Currently still learning!</p> <br>
+      <p id="p1">My Passion Is Web Development <br> I find it cool to be able to make websites! <br> This is my personal website! <br> I Hope you like it!</p>
+    </div>
+    <hr>
+    <div class="skills">
+      <h2>My Skills.</h2>
+      <div class="skill-row">
+        <img class="code-img" src="images/code.png" alt="">
+        <h3>Coding</h3>
+        <p class="l1">I love coding. It's a very interesting field. I'm still currently learning though.</p>
+      </div>
+      <div class="skill-row">
+        <img class="chilli-img" src="images/dart.png" alt="">
+        <h3>Current Challenge</h3>
+        <p class="l2">Learning as much as I can about HTML, CSS, JS, and maybe PHP in the future.</p>
+      </div>   
+    </div>
+    <div class="skill-row">
+        <img class="code-img" src="images/sport.png" alt="">
+        <h3>Soccer & Sports</h3>
+        <p class="l1">I'm pretty skillful when it comes to sports, especially soccer. <br>I also find basketball a pretty interesting sport. I like sport in general but soccer and basketball are currently my favourite.</p>
+
+    <hr>
+    <div class="contact-me">
+      <h2>Get In Touch</h2>
+      <h3>If you're interested in contacting me.</h3>
+      <p>Let's talk! let me know what's in your mind.</p>
+      <button class="bt1">Contact me</button>
+    </div>
+  </div>
+  
+  
+  <div class="bottom-container">
+    <a class="btn" href="mailto:nour@elmokhtari.com">Email Contact</a>
+    <a class="footer-link" href="https://www.instagram.com">Instagram</a>
+    <a class="footer-link" href="https://twitter.com/">Twitter</a>
+    <p class="copyright">© 2021 Nour El Mokhtari @ Nirestyle.</p>
+  </div>
+  
+</body>
+?>
